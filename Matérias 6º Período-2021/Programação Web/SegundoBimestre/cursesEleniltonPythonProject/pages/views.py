5238c09cd5654348c7f010e4dde2bad880419546
Sandrolax <sandro_pauloco@hotmail.com>
2021-10-12 14:06:53 -0300

    finalizando projeto cursos

--- Matérias 6º Período-2021/Programação Web/SegundoBimestre/cursesEleniltonPythonProject/pages/views.py
+++ Matérias 6º Período-2021/Programação Web/SegundoBimestre/cursesEleniltonPythonProject/pages/views.py
@@ -5,3 +5,96 @@ def index(request):
 
 def administracao(request):
     return render(request, 'pages/administracao.html')
+
+def agronomia(request):
+    return render(request, 'pages/agronomia.html')
+
+def arquitetura(request):
+    return render(request, 'pages/arquitetura.html')
+
+def cienciasBiologicas(request):
+    return render(request, 'pages/ciencias-biologicas.html')
+
+def cienciasContabeis(request):
+    return render(request, 'pages/ciencias-contabeis.html')
+
+def designGrafico(request):
+    return render(request, 'pages/design-grafico.html')
+
+def direito(request):
+    return render(request, 'pages/direito.html')
+
+def educacaoFisica(request):
+    return render(request, 'pages/educacao-fisica.html')
+
+def enfermagem(request):
+    return render(request, 'pages/enfermagem.html')
+
+def engenhariaCivil(request):
+    return render(request, 'pages/engenharia-civil.html')
+
+def engenhariaDeControleAutomacao(request):
+    return render(request, 'pages/engenharia-de-controle-e-automacao.html')
+
+def engenhariaSoftware(request):
+    return render(request, 'pages/engenharia-software.html')
+
+def engenhariaEletrica(request):
+    return render(request, 'pages/engenharia-eletrica.html')
+
+def engenhariaMecanica(request):
+    return render(request, 'pages/engenharia-mecanica.html')
+
+def mecatronica(request):
+    return render(request, 'pages/mecatronica.html')
+
+def farmacia(request):
+    return render(request, 'pages/farmacia.html')
+
+def fisioterapia(request):
+    return render(request, 'pages/fisioterapia.html')
+
+def fonoaudiologia(request):
+    return render(request, 'pages/fonoaudiologia.html')
+
+def fotografia(request):
+    return render(request, 'pages/fotografia.html')
+
+def historia(request):
+    return render(request, 'pages/historia.html')
+
+def jornalismo(request):
+    return render(request, 'pages/jornalismo.html')
+
+def letrasEspanhol(request):
+    return render(request, 'pages/letras-espanhol.html')
+
+def letras(request):
+    return render(request, 'pages/letras.html')
+
+def medicina(request):
+    return render(request, 'pages/medicina.html')
+
+def medicinaVeterinaria(request):
+    return render(request, 'pages/medicina-veterinaria.html')
+
+def nutricao(request):
+    return render(request, 'pages/nutricao.html')
+
+def pedagogia(request):
+    return render(request, 'pages/pedagogia.html')
+
+def producaoAudiovisual(request):
+    return render(request, 'pages/producao-audiovisual.html')
+
+def producaoMultimidia(request):
+    return render(request, 'pages/producao-multimidia.html')
+
+def psicologia(request):
+    return render(request, 'pages/psicologia.html')
+
+def publicidadePropaganda(request):
+    return render(request, 'pages/publicidade-e-propaganda.html')
+
+def sistemasInformacao(request):
+    return render(request, 'pages/sistemas-de-informacao.html')
